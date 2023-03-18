fe7faa5ce6b15145e4b1ac8d9f56dd03ce37f5e8
Yanxin Xiang <yanxinxiang0917@outlook.com>
2023-03-18 10:07:21 -0500

    1

--- enum_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-enum class people{woman,man,kids};
-int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  return 0;
-}
--- using_test.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using std::cout;
-using std::cin;
-int main(){
-    cout<<"a"<<'\n';
-}
