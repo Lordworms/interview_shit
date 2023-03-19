c6495908fbcc09f9cc6e6a9dff584f21cfd16f1f
Yanxin Xiang <yanxinxiang0917@outlook.com>
2023-03-19 15:36:57 -0500

    init

--- smart_ptr/unique_ptr_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-  unique_ptr<int>up(new int(3));
-  //all wrong since it does not support the two oeprations
-//   unique_ptr<int>p(up);
-//   unique_ptr<int>k=up;
-  return 0;
-}
--- stl/array_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-  array<int,3>a;
-  return 0;
-}
